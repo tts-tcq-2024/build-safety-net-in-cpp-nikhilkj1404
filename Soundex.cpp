f14fc4f9edc2416faffb7aee1a73103e0f7c4f6d
nikhilkj1404 <nikhilkj0414@gmail.com>
2024-07-02 00:15:16 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -22,21 +22,43 @@ char getSoundexCode(char c) {
     }
 }
 
-std::string generateSoundex(const std::string& name) {
-    if (name.empty()) return "";
+char getFirstCharacterCode(const std::string& name) {
+    if (name.empty()) return '\0';
+    return toupper(name[0]);
+}
 
+std::string processName(const std::string& name, char firstChar) {
     std::string soundex;
-    soundex += toupper(name[0]);
-    char prevCode = getSoundexCode(name[0]);
+    soundex += firstChar;
+    char prevCode = getSoundexCode(firstChar);
 
-    for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
+    // Iterate over the remaining characters of the name
+    for (size_t i = 1; i < name.length(); ++i) {
         char code = getSoundexCode(name[i]);
         if (code != '0' && code != prevCode) {
             soundex += code;
+            if (soundex.length() >= 4) {
+                break; // Stop once we have 4 characters in soundex
+            }
             prevCode = code;
         }
     }
 
-    soundex.resize(4, '0');
     return soundex;
+}
+
+std::string padSoundex(const std::string& soundex) {
+    std::string paddedSoundex = soundex;
+    paddedSoundex.resize(4, '0');
+    return paddedSoundex;
+}
+
+std::string generateSoundex(const std::string& name) {
+    if (name.empty()) return "";
+
+    char firstChar = getFirstCharacterCode(name);
+    std::string processedName = processName(name, firstChar);
+    std::string paddedSoundex = padSoundex(processedName);
+
+    return paddedSoundex;
 }
