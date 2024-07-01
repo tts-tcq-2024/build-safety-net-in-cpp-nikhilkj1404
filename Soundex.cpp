efebe755a57adbc71ee79707d2c7e1a7875367f8
nikhilkj1404 <nikhilkj0414@gmail.com>
2024-07-02 00:08:37 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -37,9 +37,6 @@ std::string generateSoundex(const std::string& name) {
         }
     }
 
-    while (soundex.length() < 4) {
-        soundex += '0';
-    }
-
+    soundex.resize(4, '0');
     return soundex;
 }
